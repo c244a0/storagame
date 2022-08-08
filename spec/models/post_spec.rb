306e5b0e4800deed3a92894aa3ccfba80fb5f8e0
c244a0 <jadegqu175@gmail.com>
2022-08-08 18:36:21 +0900

    動画投稿機能の追加バリデーション

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe Post, type: :model do
         @post.valid?
         expect(@post.errors.full_messages).to include("Title can't be blank")
       end
+      it 'titleが50文字を超えていると投稿できない' do
+        @post.title = Faker::Internet.password(min_length: 52)
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title is too long (maximum is 50 characters)")
+      end
       it 'youtube_urlがないと投稿できない' do
         @post.youtube_url = ''
         @post.valid?
@@ -25,6 +30,11 @@ RSpec.describe Post, type: :model do
         @post.content = ''
         @post.valid?
         expect(@post.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'contentが2000文字を超えていると投稿できない' do
+        @post.content = Faker::Internet.password(min_length: 2001)
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Content is too long (maximum is 2000 characters)")
       end
       it 'user_idがないと投稿できない' do
         @post.user = nil
