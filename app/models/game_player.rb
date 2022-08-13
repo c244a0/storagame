f0f61d8fa82907b4f0560cd3696912f5c7a09a80
c244a0 <jadegqu175@gmail.com>
2022-08-13 22:45:19 +0900

    モデル修正前

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,10 +1,11 @@
 class GamePlayer < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  belongs_to :grade
+  belongs_to :grade, optional: false
   with_options presence: true do
     validates :user_id
     validates :game_id
     validates :grade_id
   end
+  validates :user_id,  uniquness: { scope: [:game_id]  }
 end
