0dc26f1729c5af7047690180a33b8ba08d06323e
c244a0 <jadegqu175@gmail.com>
2022-08-16 19:08:56 +0900

    動画編集機能

--- app/javascript/darkmode.js
+++ app/javascript/darkmode.js
@@ -3,13 +3,14 @@ $(document).on('turbolinks:load', function() {
     $(".posts-body").css('background-color', '#1e1e1e');
     $(".posts-body").css('color', 'white');
     $(".youtube-title").find("a").css('color', 'white');
+    $(".post-show").find("a").css('color', 'white');
     $(this).css('display', 'none');
     $('#light').css('display', 'block');
   });
   $("#light").on('click',function(){
     $(".posts-body").css('background-color', 'white');
     $(".posts-body").css('color', 'black');
-    $(".youtube-title").find("a").css('color', 'black');
+    $(".post-show").find("a").css('color', 'black');
     $(this).css('display', 'none');
     $('#dark').css('display', 'block');
   });
