e324408f52f674d65a6e10086acaa29b25cd6437
c244a0 <jadegqu175@gmail.com>
2022-08-21 22:38:11 +0900

    ダークモード追加

--- app/javascript/darkmode.js
+++ app/javascript/darkmode.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function() {
   $("#dark").on('click',function(){
     $(".posts-body").css('background-color', '#1e1e1e');
+    $(".post-show-content-area").css('background-color', '#1e1e1e');
     $(".posts-body").css('color', 'white');
     $(".youtube-title").find("a").css('color', 'white');
     $(".youtube-cotainer").css('background-color', '#1e1e1e');
@@ -10,6 +11,7 @@ $(document).on('turbolinks:load', function() {
   });
   $("#light").on('click',function(){
     $(".posts-body").css('background-color', 'white');
+    $(".post-show-content-area").css('background-color', 'white');
     $(".posts-body").css('color', 'black');
     $(".post-show").find("a").css('color', 'black');
     $(".youtube-cotainer").css('background-color', 'white');
