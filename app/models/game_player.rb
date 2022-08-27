1c8f4e408afb82538bc8922f3f56bf500d67cfd8
c244a0 <jadegqu175@gmail.com>
2022-08-28 01:04:50 +0900

    コメントアウト記述

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,6 +1,6 @@
 class GamePlayer < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  belongs_to :grade, optional: false
-  validates :user_id, uniqueness: { scope: [:game_id] }
+  belongs_to :grade
+  validates :user_id, uniqueness: { scope: [:game_id] } # 同じユーザーに同じゲーム名の階級を複数登録するのを防ぐため。
 end
