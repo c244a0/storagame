e431ef923908c8c4c3f6e613e7e3e931708fb6a3
c244a0 <jadegqu175@gmail.com>
2022-08-28 16:13:21 +0900

    SQL軽量化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.order('id DESC').limit(100)
+    @posts = Post.includes(:game, :grade, :user)
   end
 
   def new
