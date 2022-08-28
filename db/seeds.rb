75cea1a4778d97dbe1d3f559f193241c70279243
c244a0 <jadegqu175@gmail.com>
2022-08-28 13:50:27 +0900

    コメントアウト記入

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+# ゲーム名のデータ
 Game.create(:game_title => 'マリオカート')
 Game.create(:game_title => 'ポケモン')
 Game.create(:game_title => 'クラッシュロワイヤル')
+# 階級のデータ
 Grade.create(:grades => '初級')
 Grade.create(:grades => '中級')
 Grade.create(:grades => '上級')
