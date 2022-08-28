093c8985a9402ee62b3a7b1baf4b2ed2c0ee2fb4
c244a0 <jadegqu175@gmail.com>
2022-08-28 23:52:40 +0900

    DB修正

--- db/migrate/20220808120504_create_posts.rb
+++ db/migrate/20220808120504_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.string :youtube_url 
+      t.string :youtube_url, null: false
       t.text   :content, null: false
       t.references :user, null: false, foreign_key: true
       t.references :game, null: false, foreign_key: true
