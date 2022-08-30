2de8dc09762952acc2922de1c4d5afdc4871958c
c244a0 <jadegqu175@gmail.com>
2022-08-30 23:35:39 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     @games = Game.all
     @grades = Grade.all
   end
-  
+
   def basic_auth # Basic認証
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
