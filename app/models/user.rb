285c984c9fd3cf2411671c381636a98c5dd8c445
c244a0 <jadegqu175@gmail.com>
2022-09-01 17:20:26 +0900

    ユーザー編集機能のバグの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,9 @@ class User < ApplicationRecord
   def is_followed_by?(user)
     reverse_of_relationships.find_by(following_id: user.id).present?
   end
-  has_many :game_player
-  has_many :game, through: :game_players, dependent: :destroy
-  has_many :grades, through: :game_players, dependent: :destroy
+  has_many :game_player, dependent: :destroy
+  has_many :game, through: :game_players
+  has_many :grades, through: :game_players
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
