b5d752bf4b95a49c52a52c2abcd8d05525d1e238
c244a0 <jadegqu175@gmail.com>
2022-09-05 00:56:18 +0900

    時間表示機能の実装

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Storagame
     # エラーメッセージ、ユーザー登録、サインイン等の日本語化
     config.i18n.default_locale = :ja 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Asia/Tokyo'
   end
 end
